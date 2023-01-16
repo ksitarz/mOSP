00d7162b69b1899378be1bf049e70ddd2da7a116
Dziemniak <sitarz.karol@gmail.com>
2023-01-16 23:33:34 +0100

    Add User and Role entities

---
+++ mOSP.Domain/Entities/Role.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mOSP.Domain.Entities
+{
+    internal class Role
+    {
+    }
+}
